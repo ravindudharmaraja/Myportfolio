66f682655122546b5487e212955bb89df48e7fc3
Ravindu Dharmaraja <105071596+ravindudharmaraja@users.noreply.github.com>
2022-12-11 02:06:34 +0530

    Add files via upload

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import './Portfolio.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import Untitled1 from '../../img/Untitled1.png';
+import Untitled2 from '../../img/Untitled2.png';
+import Untitled3 from '../../img/Untitled3.png';
+import { themeContext } from '../../Context';
+const Portfolio = () => {
+    const theme = useContext(themeContext);
+    const darkMode = theme.state.darkMode;
+    return (
+        <div className='portfolio' id='portfolio'>
+            {/* heading */}
+            <span>Recent Projects</span>
+            <span>Portfolio</span>
+
+            {/* slider */}
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={2}
+                grabCursor={true}
+                className='portfolio-slider'>
+                <SwiperSlide>
+                    <img src={Untitled1} alt='' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Untitled2} alt='' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Untitled3} alt='' />
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Portfolio;
